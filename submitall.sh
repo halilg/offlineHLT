2989949000f4236df9ca514f68f17debbfeadadc
DogukanL13 <dogukankizbay@gmail.com>
2018-07-31 19:53:31 +0300

    Update submitall.sh

--- submitall.sh
+++ submitall.sh
@@ -1,7 +1,10 @@
-crab submit -c crab_MET_hlt-v1x_2018A-v1.py
-crab submit -c crab_MET_hlt-v20_2018A-v1.py
-crab submit -c crab_MET_hlt-v20_2018A-v2.py
-crab submit -c crab_MET_hlt-v21_2018A-v2.py
-crab submit -c crab_MET_hlt-v21_2018A-v3.py
-crab submit -c crab_MET_hlt-v21_2018B-v1.py
+#EGamma JetHT MET SingleMuon 
+pd=$1
+crab submit -c crab_${pd}_hlt-v1x_2018A-v1.py
+crab submit -c crab_${pd}_hlt-v20_2018A-v1.py
+crab submit -c crab_${pd}_hlt-v20_2018A-v2.py
+crab submit -c crab_${pd}_hlt-v21_2018A-v2.py
+crab submit -c crab_${pd}_hlt-v21_2018A-v3.py
+crab submit -c crab_${pd}_hlt-v21_2018B-v1.py
+crab submit -c crab_${pd}_hlt-v22_2018B-v1.py
 
