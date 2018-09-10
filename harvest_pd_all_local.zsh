d587c13e001181a2823068df18bf116d6fb844c5
DogukanL13 <dogukankizbay@gmail.com>
2018-09-10 11:26:29 +0300

    Update harvest_pd_all_local.zsh

--- harvest_pd_all_local.zsh
+++ harvest_pd_all_local.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 #
-asd=(hlt-v1x hlt-v20 hlt-v21 hlt-v22 hlt-v30 hlt-v31 2018A 2018B 2018C)
+asd=(hlt-v1x hlt-v20 hlt-v21 hlt-v22 hlt-v30 hlt-v31 hlt-v33 hlt-v34 2018A 2018B 2018C 2018D)
 
 set -x
 for q in $asd; do
